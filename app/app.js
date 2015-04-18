f90d677e1f0c4c45e9e2201c7b80ed9f499f5abb
Nizar Oukhchi <nizar.oukhchi@gmail.com>
2015-04-18 14:26:02 +0100

    no message

--- app/app.js
+++ app/app.js
@@ -1,49 +1,58 @@
 var app = angular.module('app', ['ui.router', 'ngAnimate']);
 
 app.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/home");
-    $stateProvider
-        .state('home', {
-            url: "/home",
-            templateUrl: "app/partials/home.html",
-            controller: "homeCtrl"
-        })
-        .state('info', {
-            url: "/info",
-            templateUrl: "app/partials/info.html",
-            controller: "infoCtrl"
-        })
+	$urlRouterProvider.otherwise("/home");
+	$stateProvider
+		.state('home', {
+			url: "/home",
+			templateUrl: "app/partials/home.html",
+			controller: "homeCtrl"
+		})
+		.state('info', {
+			url: "/info/:cube",
+			templateUrl: "app/partials/info.html",
+			controller: "infoCtrl"
+		})
 });
 
-app.directive('drag', function() {
-    return {
-            restrict: 'A',
-            link: function(scope, el, attrs, controller) {
-                el.dragon();
-            }
-        }
+app.run(['$state', '$rootScope', function ($state, $rootScope) {
+	$rootScope.$state = $state;
+}]);
+
+
+app.directive('drag', function () {
+	return {
+		restrict: 'A',
+		link: function (scope, el, attrs, controller) {
+			el.dragon();
+		}
+	}
 });
 
-app.controller('homeCtrl', ['$scope', function ($scope) {
-    $scope.inMotionCube1 = false;
-    $scope.inMotionCube2 = false;
-    $scope.inMotionCube3 = false;
-    $scope.inMotionCube4 = false;
-    $scope.inMotionCube5 = false;
+app.controller('homeCtrl', ['$scope', '$state', function ($scope, $state) {
+	$scope.inMotionCube1 = false;
+	$scope.inMotionCube2 = false;
+	$scope.inMotionCube3 = false;
+	$scope.inMotionCube4 = false;
+	$scope.inMotionCube5 = false;
 
-    $scope.cubeClicked = function (cube) {
-        if (cube == 1) {
-            $scope.inMotionCube1 = true;
-        } else if (cube == 2) {
-            $scope.inMotionCube2 = true;
-        } else if (cube == 3) {
-            $scope.inMotionCube3 = true;
-        } else if (cube == 4) {
-            $scope.inMotionCube4 = true;
-        } else if (cube == 5) {
-            $scope.inMotionCube5 = true;
-        }
-    }
+	$scope.cubeClicked = function (cube) {
+		$state.go('info', {
+			cube: cube
+		})
+
+		//        if (cube == 1) {
+		//            $scope.inMotionCube1 = true;
+		//        } else if (cube == 2) {
+		//            $scope.inMotionCube2 = true;
+		//        } else if (cube == 3) {
+		//            $scope.inMotionCube3 = true;
+		//        } else if (cube == 4) {
+		//            $scope.inMotionCube4 = true;
+		//        } else if (cube == 5) {
+		//            $scope.inMotionCube5 = true;
+		//        }
+	}
 }]);
 
 app.controller('infoCtrl', ['$scope', function ($scope) {
