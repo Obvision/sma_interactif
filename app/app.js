16e36f54fa2e13d8be137be9d14fd39ccfffdf4b
Mehdaoui Badr <mehdaoui.badr@gmail.com>
2015-04-18 15:24:07 +0100

    Infos

--- app/app.js
+++ app/app.js
@@ -1,32 +1,32 @@
 var app = angular.module('app', ['ui.router', 'ngAnimate', 'ngTouch']);
 
 app.config(function ($stateProvider, $urlRouterProvider) {
-	$urlRouterProvider.otherwise("/home");
-	$stateProvider
-		.state('home', {
-			url: "/home",
-			templateUrl: "app/partials/home.html",
-			controller: "homeCtrl"
-		})
-		.state('info', {
-			url: "/info/:cube",
-			templateUrl: "app/partials/info.html",
-			controller: "infoCtrl"
-		})
+    $urlRouterProvider.otherwise("/home");
+    $stateProvider
+        .state('home', {
+            url: "/home",
+            templateUrl: "app/partials/home.html",
+            controller: "homeCtrl"
+        })
+        .state('info', {
+            url: "/info/:cube",
+            templateUrl: "app/partials/info.html",
+            controller: "infoCtrl"
+        })
 });
 
 app.run(['$state', '$rootScope', function ($state, $rootScope) {
-	$rootScope.$state = $state;
+    $rootScope.$state = $state;
 }]);
 
 
 app.directive('drag', function () {
-	return {
-		restrict: 'A',
-		link: function (scope, el, attrs, controller) {
-			el.dragon();
-		}
-	}
+    return {
+        restrict: 'A',
+        link: function (scope, el, attrs, controller) {
+            el.dragon();
+        }
+    }
 });
 
 app.controller('coreCtrl', ['$scope', function ($scope) {
@@ -35,20 +35,31 @@ app.controller('coreCtrl', ['$scope', function ($scope) {
 
 
 app.controller('homeCtrl', ['$scope', '$state', function ($scope, $state) {
-	$scope.inMotionCube1 = false;
-	$scope.inMotionCube2 = false;
-	$scope.inMotionCube3 = false;
-	$scope.inMotionCube4 = false;
-	$scope.inMotionCube5 = false;
+    $scope.inMotionCube1 = false;
+    $scope.inMotionCube2 = false;
+    $scope.inMotionCube3 = false;
+    $scope.inMotionCube4 = false;
+    $scope.inMotionCube5 = false;
 
-	$scope.cubeClicked = function (cube) {
-		$state.go('info', {
-			cube: cube
-		})
-	}
+    $scope.cubeClicked = function (cube) {
+        $state.go('info', {
+            cube: cube
+        })
+    }
 }]);
 
 
-app.controller('infoCtrl', ['$scope', function ($scope) {
-
+app.controller('infoCtrl', ['$scope', '$stateParams', function ($scope, $stateParams) {
+    $scope.selectedCube = $stateParams.cube;
+    if ($scope.selectedCube == 1) {
+        //Text Cube Gris
+    } else if ($scope.selectedCube == 2) {
+        //Text Cube Jaune
+    } else if ($scope.selectedCube == 3) {
+        //Text Cube Orange
+    } else if ($scope.selectedCube == 4) {
+        //Text Cube Vert
+    } else if ($scope.selectedCube == 5) {
+        //Text Cube Bleu
+    }
 }]);
