460655014c337975fe01987acad4cefa59b56554
Nizar Oukhchi <nizar.oukhchi@gmail.com>
2015-04-19 16:24:13 +0100

    no message

--- app/app.js
+++ app/app.js
@@ -1,32 +1,32 @@
-var app = angular.module('app', ['ui.router', 'ngAnimate', 'ngTouch']);
+var app = angular.module('app', ['ui.router', 'ngAnimate', 'ngTouch', 'ui.utils']);
 
 app.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/home");
-    $stateProvider
-        .state('home', {
-            url: "/home",
-            templateUrl: "app/partials/home.html",
-            controller: "homeCtrl"
-        })
-        .state('info', {
-            url: "/info/:cube",
-            templateUrl: "app/partials/info.html",
-            controller: "infoCtrl"
-        })
+	$urlRouterProvider.otherwise("/home");
+	$stateProvider
+		.state('home', {
+			url: "/home",
+			templateUrl: "app/partials/home.html",
+			controller: "homeCtrl"
+		})
+		.state('info', {
+			url: "/info/:cube",
+			templateUrl: "app/partials/info.html",
+			controller: "infoCtrl"
+		})
 });
 
 app.run(['$state', '$rootScope', function ($state, $rootScope) {
-    $rootScope.$state = $state;
+	$rootScope.$state = $state;
 }]);
 
 
 app.directive('drag', function () {
-    return {
-        restrict: 'A',
-        link: function (scope, el, attrs, controller) {
-            el.dragon();
-        }
-    }
+	return {
+		restrict: 'A',
+		link: function (scope, el, attrs, controller) {
+			el.dragon();
+		}
+	}
 });
 
 app.controller('coreCtrl', ['$scope', function ($scope) {
@@ -35,31 +35,42 @@ app.controller('coreCtrl', ['$scope', function ($scope) {
 
 
 app.controller('homeCtrl', ['$scope', '$state', function ($scope, $state) {
-    $scope.inMotionCube1 = false;
-    $scope.inMotionCube2 = false;
-    $scope.inMotionCube3 = false;
-    $scope.inMotionCube4 = false;
-    $scope.inMotionCube5 = false;
+	$scope.inMotionCube1 = false;
+	$scope.inMotionCube2 = false;
+	$scope.inMotionCube3 = false;
+	$scope.inMotionCube4 = false;
+	$scope.inMotionCube5 = false;
 
-    $scope.cubeClicked = function (cube) {
-        $state.go('info', {
-            cube: cube
-        })
-    }
+	$scope.cubeClicked = function (cube) {
+		$state.go('info', {
+			cube: cube
+		})
+	}
 }]);
 
 
 app.controller('infoCtrl', ['$scope', '$stateParams', function ($scope, $stateParams) {
-    $scope.selectedCube = $stateParams.cube;
-    if ($scope.selectedCube == 1) {
-        //Text Cube Gris
-    } else if ($scope.selectedCube == 2) {
-        //Text Cube Jaune
-    } else if ($scope.selectedCube == 3) {
-        //Text Cube Orange
-    } else if ($scope.selectedCube == 4) {
-        //Text Cube Vert
-    } else if ($scope.selectedCube == 5) {
-        //Text Cube Bleu
-    }
+	var data = [
+		{
+			title: 'Maroc, là où la Culture inspire l’Art',
+			contentPath: 'app/templates/cube1_data.html'
+		},
+		{
+			title: 'Un pays ou l’hospitalité révèle un Art de vivre',
+			contentPath: 'app/templates/cube2_data.html'
+		},
+		{
+			title: 'Maroc, un Pays d’Histoire',
+			contentPath: 'app/templates/cube3_data.html'
+		},
+		{
+			title: 'Maroc, un pays de tolérance',
+			contentPath: 'app/templates/cube4_data.html'
+		},
+		{
+			title: 'Un pays moderne et  tourné vers l’avenir',
+			contentPath: 'app/templates/cube5_data.html'
+		}
+		]
+	$scope.selectedCube = data[$stateParams.cube];
 }]);
