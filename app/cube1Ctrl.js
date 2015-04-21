3c0cbc5d8af6c1520664d492a461009a4724423e
Nizar Oukhchi <nizar.oukhchi@gmail.com>
2015-04-21 11:49:16 +0100

    no message

--- app/cube1Ctrl.js
+++ app/cube1Ctrl.js
@@ -1,3 +1,18 @@
-app.controller('cube1Ctrl', ['$scope', '$state', function ($scope, $state) {
+app.controller('cube1Ctrl', ['$scope', '$state', '$timeout', function ($scope, $state, $timeout) {
+	$scope.title = false;
+	$scope.p1 = false;
+	$scope.p2 = false;
 
+	$timeout(function () {
+		$scope.title = true;
+		$timeout(function () {
+			$scope.p1 = true;
+			$timeout(function () {
+				$scope.p1 = false;
+				$timeout(function () {
+					$scope.p2 = true;
+				}, 2000);
+			}, 20000);
+		}, 2000);
+	}, 2000);
 }]);
