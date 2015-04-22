7e203151a7a1bdb05995750fc3289bb0bb6ebb7f
Nizar Oukhchi <nizar.oukhchi@gmail.com>
2015-04-22 11:33:46 +0100

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,7 @@
 var gulp = require('gulp'),
 	minifyCSS = require('gulp-minify-css'),
-	concatCss = require('gulp-concat-css');
-
-gulp.task('concat-css', function () {
-	return gulp.src('./css/*.css')
-		.pipe(concatCss("production.css"))
-		.pipe(gulp.dest('./public/'));
-});
+	concatCss = require('gulp-concat-css'),
+	concat = require('gulp-concat');
 
 gulp.task('minify-css', function () {
 	return gulp.src('./css/production.css')
@@ -16,6 +11,26 @@ gulp.task('minify-css', function () {
 		.pipe(gulp.dest('./public/production.min.css'))
 });
 
+gulp.task('concat-css', function () {
+	return gulp.src('./css/*.css')
+		.pipe(concatCss("production.css"))
+		.pipe(gulp.dest('./public/'));
+});
 
 
-gulp.task('default', ['concat-css', 'minify-css']);
+gulp.task('scripts', function () {
+	return gulp.src(['./lib/jquery.js', './lib/angular.js', './lib/hammer.js', './lib/*.js'])
+		.pipe(concat('production.js'))
+		.pipe(gulp.dest('./public/'));
+});
+
+
+gulp.task('app', function () {
+	return gulp.src('./app/*.js')
+		.pipe(concat('app.js'))
+		.pipe(gulp.dest('./public/'));
+});
+
+
+
+gulp.task('default', ['concat-css', 'minify-css', 'scripts', 'app']);
