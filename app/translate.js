0d0fdb44e0cc2d1ed21dab546861f14d9cfff289
Mehdaoui Badr <mehdaoui.badr@gmail.com>
2015-04-22 19:54:08 +0100

    Cube 2 translated

--- app/translate.js
+++ app/translate.js
@@ -11,14 +11,17 @@ app.config(['$translateProvider', function ($translateProvider) {
         'A_MONUMENTS_DU_MAROC': 'Marokko, seine Sehenswürdigkeiten und klassischen Monumente ',
         'CONTEMPORAIN_TITLE': 'Eine zeitgenössische Schöpfung, die sich vermischt und ihre eigene Handschrift schafft',
         'CONTEMPORAIN_TEXT': 'Wenn es sich um darstellende Kunst, Literatur, Kino, Videokunst, Theater, Architektur, Mode oder Musik dreht, drückt die zeitgenössische Kunst in Marokko die Widersprüche einer Gesellschaft aus, die zutiefst ihrem geschichtlichen Erbe verbunden ist, die den  Schock der Moderne durchläuft und ihre eigene Handschrift schafft.',
-        '': '',
-        '': '',
-        '': '',
-        '': '',
-        '': '',
-        '': '',
+        'CUBE2_TITLE': 'Marokko, ein land wo die Gastfreundschaft eine Lebenskunst offenbart',
+        'HOSPITALITE_TITLE': 'Ein Fleck Erde der Gastfreundschaft und der Begegnung',
+        'HOSPITALITE_TEXT': 'Gastlich und weltoffen, hat es Marokko verstanden, die Gastfreundschaft zu einem echten Lebensstil zu erheben. Eine Geste der Großzügigkeit, des Gemeinsinns und der Toleranz, die authentische Momente des Miteinanderteilens hervorgebracht hat.',
+        'TOURISME_TITLE': 'Ein Tourismus, der seine gastliche Tradition bewahrt',
+        'TOURISME_TEXT': 'Charakteristisch für den marokkanischen Tourismus ist seine Authentizität, seine Vielfalt, seine Qualität und seine Nachhaltigkeit, mit der er sich unter den Mittelmeerländern empfiehlt.',
+        'PLAN_AZUR': 'Der himmelblaue Plan',
+        'PLAN_AZUR_TEXT': 'wurde eingerichtet, um eine Reihe von wirtschaftlich tragfähigen und soziokulturellen Qualitätsunterkünften zu schaffen, die die Prinzipien der nachhaltigen Entwicklung unter den Aspekten des Umweltschutzes respektieren.',
+        'A_MINISTER_TOURISME': 'Marokkanisches Tourismusministerium',
 
         'EN_SAVOIR_PLUS': 'Mehr Informationen :',
+
     }).translations('ar', {
         'SMA_TITLE': 'أسبوع المغرب في ألمانيا',
         'CUBE1_TITLE': 'المغرب، حيث الثقافة تلهم الفن',
@@ -31,6 +34,14 @@ app.config(['$translateProvider', function ($translateProvider) {
         'A_MONUMENTS_DU_MAROC': 'المواقع الخاصة بالمآثر المغربية',
         'CONTEMPORAIN_TITLE': 'إبداع معاصر بألوان عديدة وقادر على خلق بصمته الخاصة',
         'CONTEMPORAIN_TEXT': 'سواء تعلق الأمر بالرسوم التشكيلية أو الأداب، السينما، الفيديو، المسرح، الهندسة، الموضة، الفن المغربي المعاصر يعبر عن تناقضات مجتمع يرتبط إلى تاريخ عريق لتراثه الذي يسعى نحو خلق بصمة خاصة به',
+        'CUBE2_TITLE': 'المغرب، بلد حسن الضيافة حيث الكرم يعبر عن فن العيش',
+        'HOSPITALITE_TITLE': 'أرض حسن الضيافة واللقاءات',
+        'HOSPITALITE_TEXT': 'بلد حسن الاستقبال واللقاءات، فالمغرب عرف كيف يجعل من حسن الضيافة واحدة من أهم أساسيات فن العيش، لمسات تعبر عن الكرم، التضامن والتسامح والتي تجعلكم تعيشون فيه لحظات لا مثيل لها من الإحساس بالمشاركة',
+        'TOURISME_TITLE': 'سياحة تؤكد عن عراقة تقليد الاستقبال',
+        'TOURISME_TEXT': 'السياحة المغربية رمز للأصالة، التنوع، الجودة والاستمرارية، تفرض نفسها كمرجع أساسي لبلدان البحر الأبيض المتوسط',
+        'PLAN_AZUR': 'المخطط الأزرق',
+        'PLAN_AZUR_TEXT': 'تم إدراجه لخلق مجموعة سلاسل فندقية ذات جودة عالية، والتي تحترم شروط التطور المستمر في إطار جوانبه البيئية والاقتصادية والسوسيوثقافية',
+        'A_MINISTER_TOURISME': 'وزارة السياحة المغربية',
 
         'EN_SAVOIR_PLUS': ': للمزيد من المعلومات',
     });
