c6ed3c1083ec1e1dfe270a2287dade39be8918b0
Mehdaoui Badr <mehdaoui.badr@gmail.com>
2015-04-22 18:44:19 +0100

    Cube 1 translated

--- app/translate.js
+++ app/translate.js
@@ -3,9 +3,37 @@ app.config(['$translateProvider', function ($translateProvider) {
         'SMA_TITLE': 'Marokkowoche',
         'CUBE1_TITLE': 'Marokko, wo die Kultur die Kunst inspiriert',
         'CUBE1_INTRO': 'Marokko ist ein Land, wo sich Kunst und Kultur vermischen, eine wirkliche Einladung zum Reisen, ein Gesamtkunstwerk der Begegnung, an dem sich auf vielfältige Art und Weise teilhaben lässt. ',
+        'ARTISANAT_TITLE': 'Ein Kunsthandwerk auf der Suche nach dem Schönen',
+        'ARTISANAT_TEXT': 'Ein Kunsthandwerk in tausend und einer Facette, wo das Objekt eine menschliche Dimension einnimmt und das authentischste Zeugnis der Suche nach Schönheit ist.',
+        'CUISINE_TITLE': 'Eine Kochkunst, die die Frucht verschiedener Einflüsse ist',
+        'CUISINE_TEXT': 'Anerkannt wie eine Kunst ist die marokkanische Küche die Frucht phönizischer, kathargischer, römischer, arabischer, andalusischer, türkischer, französischer Einflüsse und bleibt offen für weitere.',
+        'PATRIMOINE_TITLE': 'Ein materielles und immaterielles Erbe von großem Reichtum ',
+        'A_MONUMENTS_DU_MAROC': 'Marokko, seine Sehenswürdigkeiten und klassischen Monumente ',
+        'CONTEMPORAIN_TITLE': 'Eine zeitgenössische Schöpfung, die sich vermischt und ihre eigene Handschrift schafft',
+        'CONTEMPORAIN_TEXT': 'Wenn es sich um darstellende Kunst, Literatur, Kino, Videokunst, Theater, Architektur, Mode oder Musik dreht, drückt die zeitgenössische Kunst in Marokko die Widersprüche einer Gesellschaft aus, die zutiefst ihrem geschichtlichen Erbe verbunden ist, die den  Schock der Moderne durchläuft und ihre eigene Handschrift schafft.',
+        '': '',
+        '': '',
+        '': '',
+        '': '',
+        '': '',
+        '': '',
+
+        'EN_SAVOIR_PLUS': 'Mehr Informationen :',
     }).translations('ar', {
         'SMA_TITLE': 'أسبوع المغرب في ألمانيا',
+        'CUBE1_TITLE': 'المغرب، حيث الثقافة تلهم الفن',
+        'CUBE1_INTRO': 'المغرب بلد حيث تتماها الثقافة بالفن، إنه دعوة صريحة للسفر، عطر اللقاء الذي يمنح نفسه ليتم تقاسمه على أكثر من نقطة',
+        'ARTISANAT_TITLE': 'صناعة تقليدية في بحث مستمر على الجميل',
+        'ARTISANAT_TEXT': 'صناعة تقليدية بألف وجه ووجه، حيث الموضوع يأخذ بعده الإنساني وهو حليل أصيل على بحثها المستمر عن الجمال',
+        'CUISINE_TITLE': 'فن طبخ راق، هو باكورة مؤثرات عديدة',
+        'CUISINE_TEXT': 'اشتهر فن الطبخ المغربي كفن قائم بذاته، فهو نتاج مؤثرات عديدة فينيقية، قرطاجية، رومانية، عربية، أندلسية، تركية، فرنسية، وتبقى منفتحة على مؤثرات أخرى',
+        'PATRIMOINE_TITLE': 'تراث مادي ولا مادي، غني ومتنوع',
+        'A_MONUMENTS_DU_MAROC': 'المواقع الخاصة بالمآثر المغربية',
+        'CONTEMPORAIN_TITLE': 'إبداع معاصر بألوان عديدة وقادر على خلق بصمته الخاصة',
+        'CONTEMPORAIN_TEXT': 'سواء تعلق الأمر بالرسوم التشكيلية أو الأداب، السينما، الفيديو، المسرح، الهندسة، الموضة، الفن المغربي المعاصر يعبر عن تناقضات مجتمع يرتبط إلى تاريخ عريق لتراثه الذي يسعى نحو خلق بصمة خاصة به',
+
+        'EN_SAVOIR_PLUS': ': للمزيد من المعلومات',
     });
 
-    $translateProvider.preferredLanguage('de');
+    $translateProvider.preferredLanguage('ar');
 }]);
